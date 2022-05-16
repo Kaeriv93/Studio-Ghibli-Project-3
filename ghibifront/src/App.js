c26d6088fe1a2abc8cff92aa8f046df5c2773a49
kaeriv93 <vsan93@yahoo.com>
2022-05-16 18:48:21 -0500

    Using routes and links

--- ghibifront/src/App.js
+++ ghibifront/src/App.js
@@ -1,6 +1,8 @@
 import Header from "./Components/Header";
 import Main from "./Components/Main";
 import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
+import {Routes,Route} from 'react-router-dom'
 import List from "./Components/Test";
 import SearchContainer from "./Components/searchContainer";
 import Login from "./Login/login";
--- ghibifront/src/Components/Header.js
+++ ghibifront/src/Components/Header.js
@@ -1,16 +1,14 @@
 import React from 'react'
-// import {Routes,Route} from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
+import { BrowserRouter as Router } from "react-router-dom";
 import {Link} from 'react-router-dom'
 import Login from '../Login/login'
 
 const Header = () =>{
     return(
         <nav>
-            <Link to='/login'>
-                <div>Login</div>
-            </Link>
-            <Link to='/signup'>
-                <div>Sign Up</div>
+            <Link to ='login'>
+                Login
             </Link>
         </nav>
     )

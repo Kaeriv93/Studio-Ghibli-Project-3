ea4f45073f8b96c629e17454c7a1ae8413ccc272
arishamays1 <1arishamays1@gmail.com>
2022-05-16 12:11:46 -0400

    frontend  recreated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+import Header from "./Components/Header";
+import Main from "./Components/Main";
 import './App.css';
-
+import List from "./Components/namel8r";
+import SearchContainer from "./Components/searchContainer";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchContainer/>
+      < List/>
     </div>
   );
 }
---
+++ src/Components/namel8r.js
@@ -0,0 +1,31 @@
+import '../styles/home.css'
+import { useEffect, useState } from "react";
+
+export default function List() {
+  const [film, setFilm] = useState(null);
+
+  const URL = "https://ghibliapi.herokuapp.com/films";
+
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch(URL);
+      const data = await response.json();
+      setFilm(data);
+    };
+    getData();
+  }, []);
+
+  const loaded = () => {
+    return film.map((film, idx) => (
+      <div className='showpg' key={idx}>
+          <div className='eachMov'> 
+              <h1>{film.title}</h1>
+        <img className="gImage" src={film.image} alt={film.name} />
+        </div>
+       
+      </div>
+    ));
+  };
+
+  return film ? loaded() : <h1>Loading.......</h1>;
+}

d213243967fe48c023bc57a8fffea69c3a9d427c
kaeriv93 <vsan93@yahoo.com>
2022-05-18 14:12:31 -0500

    minor css changes

--- ghibifront/src/Signup/signup.jsx
+++ ghibifront/src/Signup/signup.jsx
@@ -60,37 +60,39 @@ const Signup = (props) =>{
 
 
     return(
-        <div className="signup">
-            <form onSubmit={handleSubmit} autoComplete="off">
-                <div className="grid-container">
-                    <div className="sign-container">
-                        <div className="sign">
-                            <h1>Sign Up</h1>
-                            <p>Please fill in this form to create an account</p>
-                        </div>
-                        <hr/>
-                        <label for="firstName"><b>First Name</b></label>
-                        <input id="signing" type="text" value={newForm.firstName} placeholder="Enter your first name" name="firstName" required onChange={handleChange} />
-                        <br/>
-                        <label for ="lastName"><b>Last Name</b></label>
-                        <input id="signing" type="text" value={newForm.lastName} placeholder="Enter your last name" name="lastName" required onChange={handleChange}/>
-                        <br/>
-                        <label for ="username"><b>Username</b></label>
-                        <input id="signing" type="text" value={newForm.username} placeholder="Enter your username" name="username" required onChange={handleChange}/>
-                        <br/>
-                        <label for="email"><b>Email</b></label>
-                        <input id="signing" type="text" value={newForm.email} placeholder="Enter Email" name="email" required onChange={handleChange}/>
-                        <br/>
-                        <label for ="password"><b>Password</b></label>
-                        <input id="signing" type="password" value={newForm.password} placeholder="Enter Password" name="password" required onChange={handleChange}/>
-                        <input type="submit" value="Sign Up"/>
-                    
+        <>
+            <div className="signup">
+                <form onSubmit={handleSubmit} autoComplete="off">
+                    <div className="grid-container">
+                        <div className="sign-container">
+                            <div className="sign">
+                                <h1>Sign Up</h1>
+                                <p>Please fill in this form to create an account</p>
+                            </div>
+                            <hr/>
+                            <label for="firstName"><b>First Name</b></label>
+                            <input id="signing" type="text" value={newForm.firstName} placeholder="Enter your first name" name="firstName" required onChange={handleChange} />
+                            <br/>
+                            <label for ="lastName"><b>Last Name</b></label>
+                            <input id="signing" type="text" value={newForm.lastName} placeholder="Enter your last name" name="lastName" required onChange={handleChange}/>
+                            <br/>
+                            <label for ="username"><b>Username</b></label>
+                            <input id="signing" type="text" value={newForm.username} placeholder="Enter your username" name="username" required onChange={handleChange}/>
+                            <br/>
+                            <label for="email"><b>Email</b></label>
+                            <input id="signing" type="text" value={newForm.email} placeholder="Enter Email" name="email" required onChange={handleChange}/>
+                            <br/>
+                            <label for ="password"><b>Password</b></label>
+                            <input id="signing" type="password" value={newForm.password} placeholder="Enter Password" name="password" required onChange={handleChange}/>
+                            <input type="submit" value="Sign Up"/>
+                        
 
+                        </div>
                     </div>
-                </div>
-            </form>
-            <ToastContainer/>
-        </div>
+                </form>
+                <ToastContainer/>
+            </div>
+        </>
     )
 }
 

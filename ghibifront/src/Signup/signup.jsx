1c635bbe8f5302afcfb21819f75b8a261e50c0d9
kaeriv93 <vsan93@yahoo.com>
2022-05-17 22:13:55 -0500

    added flex to signup page

--- ghibifront/src/Signup/signup.jsx
+++ ghibifront/src/Signup/signup.jsx
@@ -55,28 +55,30 @@ const Signup = (props) =>{
     return user? (
         <div className="signup">
             <form onSubmit={handleSubmit} autoComplete="off">
-                <div className="container">
-                    <div className="sign">
-                        <h1>Sign Up</h1>
-                        <p>Please fill in this form to create an account</p>
-                    </div>
-                    <hr/>
-                    <label for="firstName"><b>First Name</b></label>
-                    <input type="text" value={newForm.firstName} placeholder="Enter your first name" name="firstName" required onChange={handleChange} />
-                    <br/>
-                    <label for ="lastName"><b>Last Name</b></label>
-                    <input type="text" value={newForm.lastName} placeholder="Enter your last name" name="lastName" required onChange={handleChange}/>
-                    <br/>
-                    <label for ="username"><b>Username</b></label>
-                    <input type="text" value={newForm.username} placeholder="Enter your username" name="username" required onChange={handleChange}/>
-                    <br/>
-                    <label for="email"><b>Email</b></label>
-                    <input type="text" value={newForm.email} placeholder="Enter Email" name="email" required onChange={handleChange}/>
-                    <br/>
-                    <label for ="password"><b>Password</b></label>
-                    <input type="password" value={newForm.password} placeholder="Enter Password" name="password" required onChange={handleChange}/>
-                    <input type="submit" value="Sign Up"/>
+                <div className="grid-container">
+                    <div className="container">
+                        <div className="sign">
+                            <h1>Sign Up</h1>
+                            <p>Please fill in this form to create an account</p>
+                        </div>
+                        <hr/>
+                        <label for="firstName"><b>First Name</b></label>
+                        <input type="text" value={newForm.firstName} placeholder="Enter your first name" name="firstName" required onChange={handleChange} />
+                        <br/>
+                        <label for ="lastName"><b>Last Name</b></label>
+                        <input type="text" value={newForm.lastName} placeholder="Enter your last name" name="lastName" required onChange={handleChange}/>
+                        <br/>
+                        <label for ="username"><b>Username</b></label>
+                        <input type="text" value={newForm.username} placeholder="Enter your username" name="username" required onChange={handleChange}/>
+                        <br/>
+                        <label for="email"><b>Email</b></label>
+                        <input type="text" value={newForm.email} placeholder="Enter Email" name="email" required onChange={handleChange}/>
+                        <br/>
+                        <label for ="password"><b>Password</b></label>
+                        <input type="password" value={newForm.password} placeholder="Enter Password" name="password" required onChange={handleChange}/>
+                        <input type="submit" value="Sign Up"/>
 
+                    </div>
                 </div>
             </form>
             

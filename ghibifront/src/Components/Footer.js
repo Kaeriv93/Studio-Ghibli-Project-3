41d04013977c45094285e541c3bf993c086c1cfb
kaeriv93 <vsan93@yahoo.com>
2022-05-18 15:17:33 -0500

    walking normally

--- ghibifront/src/Components/Footer.js
+++ ghibifront/src/Components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () =>{
     return(
         <div>
             <div className="totoro"> 
-                <img src="https://64.media.tumblr.com/dbde3eb8a96b0960464e3e06ee5ab4df/tumblr_pfaijsOChg1u2yfqbo1_500.gifv" width="100px" height="100px" alt="totoro walking"/> 
+                <img src="https://i.imgur.com/IAWXSei.gif" width="100px" height="100px" alt="totoro walking"/> 
             </div>
             <div className="footer">
                 © General Assembly
